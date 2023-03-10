77e2ca90ab201a6461c43da6a301685081f18222
Muhammad Anwar Subhani <rajaanwar803@gmail.com>
2023-03-10 22:34:11 +0500

    calculator done

---
+++ cli_calculator/calculator.ts
@@ -0,0 +1,43 @@
+
+function add(num1: number, num2: number): number {
+    return num1 + num2;
+}
+
+function subtract(num1: number, num2: number): number {
+    return num1 - num2;
+}
+
+function multiply(num1: number, num2: number): number {
+    return num1 * num2;
+}
+
+function divide(num1: number, num2: number): number {
+    return num1 / num2;
+}
+
+
+
+let firstNum = prompt("Enter first number: ");
+let operation = prompt("Enter your operation (+ - * /): ");
+let secondNum = prompt("Enter second number: ");
+
+firstNum = parseFloat(firstNum);
+secondNum = parseFloat(secondNum);
+
+if (operation == "+") {
+    let result = add(firstNum, secondNum)
+    console.log(`${firstNum} + ${secondNum} = ${result}`);
+}
+else if (operation == "-") {
+    let result = subtract(firstNum, secondNum)
+    console.log(`${firstNum} - ${secondNum} = ${result}`);
+}
+else if (operation == "*") {
+    let result = multiply(firstNum, secondNum)
+    console.log(`${firstNum} * ${secondNum} = ${result}`);
+}
+else if (operation == "/") {
+    let result = divide(firstNum, secondNum)
+    console.log(`${firstNum} / ${secondNum} = ${result}`);
+}
+
